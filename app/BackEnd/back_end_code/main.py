5ed7f8ebafa8a2e71b9fb3bb07d32b0e611ae1a8
RanyaYaseenBring <Ranya.Yaseen@bring.com>
2026-05-18 13:39:15 +0200

    andere commit

--- app/BackEnd/back_end_code/main.py
+++ app/BackEnd/back_end_code/main.py
@@ -181,9 +181,7 @@ def answer_question(question: str, session_id: str):
 
         prompt = f"""
 You are a parser.
-
 You are NOT a chatbot.
-
 Return ONLY one of these formats:
 
 employee_lookup|name|field
@@ -286,57 +284,6 @@ USER MESSAGE:
         .replace("\r", "")
         .strip()
     )
-
-    TABLE_GROUPS = {
-
-    "employees": [
-        "afas.Bring_Employees",
-        "afas.Profit_Employees",
-        "afas.Profit_Users",
-        "afas.Profit_UserGroups"
-    ],
-
-    "employee_details": [
-        "afas.Bring_Medewerker_Functie",
-        "afas.Bring_Medewerker_Contract",
-        "afas.Bring_Medewerker_Salaris",
-        "afas.Bring_Verzuim",
-        "afas.Bring_LeaveTypes",
-        "afas.Profit_LeaveBalance"
-    ],
-
-    "finance": [
-        "afas.Bring_FinancieleMutaties_ESJ",
-        "afas.Bring_Debtor_Invoices",
-        "afas.Profit_Debtor",
-        "afas.Profit_Journals"
-    ],
-
-    "organization": [
-        "afas.Bring_KNOrganisation"
-    ],
-
-    "workflow": [
-        "afas.Bring_workflow_log"
-    ],
-
-    "logs": [
-        "_logs.ProcesssingLog",
-        "_logs.EtlRunLog"
-    ],
-
-    "system": [
-        "_etl.APITokens",
-        "_etl.Data_processor",
-        "_etl.EndpointConfig"
-    ],
-
-    "LSP": [
-        "dbo.Tally",
-        "LSP_NL.bcnl_edirefs",
-        "LSP_NL.peppol_lijn12"
-    ]
-}
 
     if clean_result.lower().startswith("employee_lookup|"):
 
