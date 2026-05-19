555f4e0a7569250f546b49fadb321838484e4ca3
RanyaYaseenBring <Ranya.Yaseen@bring.com>
2026-05-19 13:28:44 +0200

    docker

--- my-react-app/src/api/askBackend.js
+++ my-react-app/src/api/askBackend.js
@@ -1,16 +1,20 @@
+const API_URL = import.meta.env.VITE_API_URL;
 export async function askBackend(topic, question, sessionId) {
-  const response = await fetch("http://172.20.20.68:8000/ask"
-, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      topic: topic,
-      question: question,
-      session_id: sessionId
-    }),
-  });
+
+  const response = await fetch(
+    `${API_URL}/ask`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        topic: topic,
+        question: question,
+        session_id: sessionId
+      }),
+    }
+  );
 
   return await response.json();
 }
